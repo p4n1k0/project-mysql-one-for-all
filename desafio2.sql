96d32dfcb009166d76a548d65a01eb6d9ade7a7a
Tarick <gabre.tarick@gmail.com>
2022-08-17 00:38:38 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT COUNT(DISTINCT c.cantion_id) AS cancoes,
+	   COUNT(DISTINCT al.album_id) AS albuns, 
+       COUNT(DISTINCT a.artist_id) AS artistas        
+FROM SpotifyClone.cantion AS c
+INNER JOIN SpotifyClone.album AS al 
+ON c.album_id = al.album_id
+INNER JOIN SpotifyClone.artist AS a
+ON al.artist_id = a.artist_id;
