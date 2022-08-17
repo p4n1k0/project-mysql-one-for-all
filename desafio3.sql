8dd72521e33151fd44ec85565b71a7edaec5ed9e
Tarick <gabre.tarick@gmail.com>
2022-08-17 12:50:16 -0300

    desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,11 +1,10 @@
-SELECT
-  u.username AS usuario,
-  COUNT(ucd.cantion_id) AS qt_de_musicas_ouvidas,
-  ROUND((SUM(c.length)/60), 2) AS total_minutos 
-FROM SpotifyClone.user AS u
-INNER JOIN SpotifyClone.user_cantion_date AS ucd 
-ON u.user_id = ucd.user_id
-INNER JOIN SpotifyClone.cantion AS c 
+SELECT u.username AS usuario, 
+        COUNT(c.name) AS qt_de_musicas_ouvidas,
+        ROUND(SUM(c.length)/60,2) AS total_minutos
+     FROM SpotifyClone.user_cantion_date AS ucd
+INNER JOIN SpotifyClone.user AS u
+ON ucd.user_id = u.user_id
+INNER JOIN SpotifyClone.cantion AS c
 ON ucd.cantion_id = c.cantion_id
-GROUP BY u.user_id
-ORDER BY u.username; 
+GROUP BY usuario
+ORDER BY usuario;
